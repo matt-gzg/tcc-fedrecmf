a62162737e966c8074b081415604535c6b92f0b1
matt-gzg <matheus.gonzaga186@gmail.com>
2026-05-16 01:04:17 -0300

    deixando a avaliacao de um jeito menos porco

--- fedMF.py
+++ fedMF.py
@@ -47,44 +47,40 @@ if __name__ == '__main__':
     user_vector = np.zeros([len(user_id_list), hidden_dim]) + 0.01
     item_vector = np.zeros([len(item_id_list), hidden_dim]) + 0.01
 
-    precision_list = []
-    ndcg_list     = []
+    def evaluate(user_idx, uid, data):
+        items = data[uid]
+        relevant_items = set(item_id for item_id, rate, _ in items if rate >= 4)
+        p = np.dot(user_vector[user_idx], item_vector.T)
+        ranked_items =  np.argsort(p)[::-1][:k]
+        num_relevant = len(relevant_items)
 
-    #treino
+        dcg, idcg, hits = 0.0, 0.0, 0
+        for rank in range(k):
+            rel = 1 if ranked_items[rank] in relevant_items else 0
+            hits += rel
+            dcg += rel / np.log2(rank + 2)
+            if rank < num_relevant:
+                idcg += 1 / np.log2(rank + 2)
+
+        precision = hits / k
+        ndcg = dcg / idcg if idcg > 0 else 0.0
+        return precision, ndcg
+
+    #treino prequencial
     # Step 2 User updates
+    precision_list, ndcg_list = [], []
     user_time_list = []
     for uid, item_id, rate, timestamp in global_train:
         t = time.time()
-
         user_idx = user_id_to_idx[uid]
         
-        relevant_items = set(
-            iid for iid, r, _ in test_data[uid] if r >= 4
-        )
-
-        if len(relevant_items) > 0:
-            p = np.dot(user_vector[user_idx], item_vector.T)
-            ranked_items = np.argsort(p)[::-1][:k]
-
-            #precision
-            num_relevant_in_top_k = sum(1 for iid in ranked_items if iid in relevant_items)
-            precision_list.append(num_relevant_in_top_k / k)
-
-            #ndcg@k
-            dcg = sum(
-                (1 if ranked_items[rank] in relevant_items else 0) / np.log2(rank + 2)
-                for rank in range(k)
-            )
-            idcg = sum(
-                1 / np.log2(rank + 2)
-                for rank in range(min(k, len(relevant_items)))
-            )
-            ndcg_list.append(dcg / idcg if idcg > 0 else 0)
+        p, n = evaluate(user_idx, uid, test_data)
+        precision_list.append(p)
+        ndcg_list.append(n)
 
         single_rating = [(item_id, rate, timestamp)]
-        user_vector[user_idx], gradient = user_update(
-            user_vector[user_idx], single_rating, item_vector
-        )
+        user_vector[user_idx], gradient = user_update(user_vector[user_idx], single_rating, item_vector)
+        
         for iid, grad in gradient.items():
             item_vector[iid] -= grad
 
@@ -92,44 +88,18 @@ if __name__ == '__main__':
         print('User-%s update using' % uid, user_time_list[-1], 'seconds')
 
     print('User Average time', np.mean(user_time_list))
+    print('Prequential Precision@{}: {:.4f}'.format(k, np.mean(precision_list)))
+    print('Prequential NDCG@{}: {:.4f}'.format(k, np.mean(ndcg_list)))
     print('loss', loss())
     print('Costing', max(user_time_list), 'seconds')
 
-    precision_list = []
-    ndcg_list = []
+    final_precision_list, final_ndcg_list = [], []
 
     for i, uid in enumerate(user_id_list):
-        test_items = test_data[uid]
-        if len(test_items) == 0:
-            continue
-        
-        #rating >= 4
-        relevant_items = set(item_id for item_id, rate, _ in test_items if rate >= 4)
-        
-        p = np.dot(user_vector[i], item_vector.T)
-        ranked_items = np.argsort(p)[::-1][:k]
-        
-        # Precision@k
-        num_relevant_in_top_k = sum(1 for item_id in ranked_items if item_id in relevant_items)
-        precision = num_relevant_in_top_k / k
-        precision_list.append(precision)
-        
-        # NDCG@k
-        dcg = 0.0
-        idcg = 0.0
-        for rank in range(k):
-            item_id = ranked_items[rank]
-            rel = 1 if item_id in relevant_items else 0
-            dcg += rel / np.log2(rank + 2)
-        
-        num_relevant = len(relevant_items)
-        for rank in range(min(k, num_relevant)):
-            idcg += 1 / np.log2(rank + 2)
-        
-        ndcg = dcg / idcg if idcg > 0 else 0
-        ndcg_list.append(ndcg)
+        p, n = evaluate(i, uid, test_data)
+        final_precision_list.append(p)
+        final_ndcg_list.append(n)
 
     print('Precision@{}: {:.4f}'.format(k, np.mean(precision_list)))
     print('NDCG@{}: {:.4f}'.format(k, np.mean(ndcg_list)))
-
     print('Total time', time.time() - time_dataset, 'seconds')
